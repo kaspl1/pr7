317334377b7870b765b6e469a6d448659a021896
kaspl1 <ilya04cobraa@yandex.ru>
2024-10-16 00:16:50 +0500

    Добавление файлов программ

---
+++ homework2/pr7.1.py
@@ -0,0 +1,16 @@
+def main():
+    try:
+        num = int(input("Введите целое десятичное число: "))
+        if num >= 0:
+            print(f"Двоичное представление: {bin(num)[2:]}")
+            print(f"Восьмеричное представление: {oct(num)[2:]}")
+        else:
+            print(f"Двоичное представление: -{bin(abs(num))[2:]}")
+            print(f"Восьмеричное представление: -{oct(abs(num))[2:]}")
+
+    except ValueError:
+        print("Ошибка: ввод не верный.")
+
+
+if __name__ == "__main__":
+    main()
